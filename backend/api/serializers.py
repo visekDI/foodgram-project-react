3e79600c8c61fd0bde3b5fbda981ac6312e1212f
visekDI <visekDI@yandex.ru>
2023-09-17 17:43:24 +0300

    v1.4

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import transaction
 from django.db.models import F
-from django.shortcuts import get_object_or_404
 from djoser.serializers import UserSerializer
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework.exceptions import ValidationError
