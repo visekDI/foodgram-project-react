90ab2a6110956c55f8929fd96490e2bd4ff74fcf
visekDI <visekDI@yandex.ru>
2023-09-18 13:46:40 +0300

    v1.11

--- backend/api/views.py
+++ backend/api/views.py
@@ -14,6 +14,7 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+
 from recipes.models import (
     Favourite,
     Ingredient,
